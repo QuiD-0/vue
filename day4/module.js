aa9f36bed05ee0fcc0a0514a4d1d9050ca702868
QuiD <wodnd101@naver.com>
2022-05-10 13:44:47 +0900

    module

---
+++ day4/module.js
@@ -0,0 +1,26 @@
+function cube(val) {
+    return val * val * val;
+}
+
+const foo = Math.PI * 2;
+
+var tommy = {
+    num: 1,
+    name: "Tommy Lee",
+    address: "Seoul",
+};
+
+export {
+    cube,
+    tommy,
+    foo
+};
+export default {
+    id:100,
+    name:"Jane Kim",
+    phone:"010-1234-5678",
+};
+
+// export default function(a){
+//     return a+100;
+// }
---
+++ day4/yourModule.js
@@ -0,0 +1,13 @@
+import {
+    cube,
+    foo,
+    tommy,
+
+} from './module.js';
+import jane from './module.js';
+
+console.log(cube(3));
+console.log(foo);
+console.log(tommy.name, tommy.address);
+
+console.log(jane);
