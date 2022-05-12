f21ed35cbf54bfc4f3eef2b355bcffcdbfeff2ba
QuiD <wodnd101@naver.com>
2022-05-12 13:33:20 +0900

    add my component

---
+++ cli_projects/my-proj/src/components/MyWorld.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>My World.vue</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"MyWorld",
+    }
+</script>
+
+<style scoped>
+    h1{
+        color:cadetblue;
+    }
+</style>
