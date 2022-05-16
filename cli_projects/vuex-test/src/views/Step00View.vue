d689b05e757b8557f56c627ff7922367368959ba
QuiD <wodnd101@naver.com>
2022-05-16 14:48:55 +0900

    vuex test

---
+++ cli_projects/vuex-test/src/views/Step00View.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="text-center">
+    <h2>당신이 좋아하는 파트를 선택하세요</h2>
+    <result-component :total="count" />
+    <subject-component
+      v-on:addtotcount="addTotalCount"
+      title="코딩"
+    ></subject-component>
+    <subject-component
+      v-on:addtotcount="addTotalCount"
+      title="알고리즘"
+    ></subject-component>
+  </div>
+</template>
+
+<script>
+import ResultComponent from "@/components/step00/ResultComponent.vue";
+import SubjectComponent from "@/components/step00/SubjectComponent.vue";
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  components: {
+    ResultComponent,
+    SubjectComponent,
+  },
+  methods: {
+    addTotalCount() {
+      this.count += 1;
+    },
+  },
+};
+</script>
+<style scoped>
+.text-center {
+  text-align: center;
+}
+</style>
---
+++ cli_projects/vuex-test/src/views/Step05View.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center">
+    <h2>당신이 좋아하는 파트를 선택하세요</h2>
+    <result-component />
+    <subject-component title="코딩"></subject-component>
+    <subject-component title="알고리즘"></subject-component>
+  </div>
+</template>
+
+<script>
+import ResultComponent from "@/components/step05/ResultComponent.vue";
+import SubjectComponent from "@/components/step05/SubjectComponent.vue";
+export default {
+  components: {
+    ResultComponent,
+    SubjectComponent,
+  },
+};
+</script>
+<style>
+.text-center {
+  text-align: center;
+}
+</style>
