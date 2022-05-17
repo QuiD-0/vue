6f1d2817e5bd7b4e19026ed51b635140ee0f82ab
QuiD <wodnd101@naver.com>
2022-05-17 13:14:53 +0900

    Update todoDetail.vue

--- cli_projects/vuex/src/views/todoDetail.vue
+++ cli_projects/vuex/src/views/todoDetail.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <h1>Todo Detail</h1>
-    {{ todos.num }} {{ todos.id }} {{ todos.content }}
+    {{ todos.num }} {{ todos.id }} {{ todos.content }} {{ todos.done }}
     <button @click="del(todos.num)">삭제</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 /* eslint-disable */
 import axios from '../store/axios-common'
 export default {
   methods: {
+    // ...mapActions(['oneTodo']),
     selectOne(num) {
       this.$store.dispatch('oneTodo', num)
     },
