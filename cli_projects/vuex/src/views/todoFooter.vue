9573a93a784d54b790622f4983897d8afcf3907c
QuiD <wodnd101@naver.com>
2022-05-17 16:33:32 +0900

    Create todoFooter.vue

---
+++ cli_projects/vuex/src/views/todoFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import TodoFooter from './todoFooter'
+
+export default {
+  components: {
+    TodoFooter,
+  },
+}
+</script>
+
+<style></style>
